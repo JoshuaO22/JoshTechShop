589a3ef3867357499b6b37a41f7730f89bdb30a8
Joshua Odom <joshuaodom23@outlook.com>
2026-06-11 18:21:04 -0400

    update script

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Developer: Joshua Odom
 // Website link: https://joshuao22.github.io/JoshTechShop/
 // Source Link: https://github.com/JoshuaO22/JoshTechShop
+const requests = [];
+const logins = [];
 
 const requestForm = document.getElementById('request-form');
 const adminLoginForm = document.getElementById('admin-login-form');
@@ -17,7 +19,9 @@ requestForm.addEventListener('submit', function (event) {
         message: document.getElementById('description').value
     }
 
+    requests.push(request);
     console.log(request);
+    requestForm.reset();
 });
 
 adminLoginForm.addEventListener('submit', function (event) {
@@ -28,5 +32,7 @@ adminLoginForm.addEventListener('submit', function (event) {
         password: document.getElementById('password').value
     }
 
+    logins.push(loginData);
     console.log(loginData);
+    adminLoginForm.reset();
 });
